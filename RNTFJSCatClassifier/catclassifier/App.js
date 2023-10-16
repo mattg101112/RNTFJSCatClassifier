bb57e5c672949c7b488f8d3e007d88d91b142d33
mattg101112 <147451520+mattg101112@users.noreply.github.com>
2023-10-16 15:03:24 -0500

    Camera seems to be working
    Camera seems to be working

--- RNTFJSCatClassifier/catclassifier/App.js
+++ RNTFJSCatClassifier/catclassifier/App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import CameraScreen from './CameraScreen';
 
 function HomeScreen({ navigation } ) {
   return (
@@ -29,7 +30,7 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="CameraScreen" component={CameraScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ RNTFJSCatClassifier/catclassifier/CameraScreen.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button } from 'react-native';
+import { Camera } from 'expo-camera';
+
+const CameraScreen = ({ navigation }) => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const takePicture = async () => {
+    if (camera) {
+      const photo = await camera.takePictureAsync();
+      navigation.navigate('ImageScreen', { photo });
+    }
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      <Camera
+        ref={(ref) => setCamera(ref)}
+        style={{ flex: 1 }}
+        type={Camera.Constants.Type.back}
+      />
+      <Button title="Take Picture" onPress={takePicture} />
+    </View>
+  );
+};
+
+export default CameraScreen;
